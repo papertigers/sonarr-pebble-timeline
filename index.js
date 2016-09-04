92e35efa56193c85134528dafbfa58b648367fc8
Mike Zeller <mike@mikezeller.net>
2016-09-04 18:21:15 +0000

    Setup db and basic registration handling

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var bunyan = require('bunyan');
 var restify = require('restify');
+var PouchDB = require('pouchdb');
 
 var app = require('./lib').app
 var config = require('./config');
+var userdb = new PouchDB('users');
+config.server.userdb = userdb;
 
 function run() {
-  LOG = bunyan.createLogger({
+
+  log = bunyan.createLogger({
     level: ('info'),
     name: config.server.name,
     stream: process.stdout,
     serializers: restify.bunyan.serializers
-});
+  });
 
   return app.createServer(config.server, function (server) {
     server.start(function () {
-      LOG.info('server listening at %s', server.url);
+      log.info('server listening at %s', server.url);
     });
 
     return server;
